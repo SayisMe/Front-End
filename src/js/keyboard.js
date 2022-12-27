d0e5ab336f6b6bbc747b327c11d3461484ea7406
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2022-12-27 15:01:55 +0900

    Update keyboard.js

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -17,32 +17,39 @@ export class Keyboard {
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
     this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
     this.#inputGrouptEl = this.#containerEl.querySelector("#input-group");
-    this.#inputEl = this.#inputGrouptEl.querySelector("#input");
     // document.getElemntById("switch") 로 탐색할 수 있지만, 그보다 하위인 container 부터 탐색하도록 해서 비용을 절감할 수 있게 되었다.
   }
 
   #addEvent() {
     this.#switchEl.addEventListener("change", this.#onChangeTheme);
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
-    document.addEventListener("keydown",(event) => {
-      this.#inputGrouptEl.classList.toggle(
-        "error",
-        /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
-      );
-      
-      this.#keyboardEl
-        .querySelector(`[data-code=${event.code}]`)
-        ?.classList.add("active");
-    });
-    document.addEventListener("keyup", (event) => {
-      //console.log("keyup")
-      this.#keyboardEl
-        .querySelector(`[data-code=${event.code}]`)
-        ?.classList.remove("active");
-    });
-    this.#inputEl.addEventListener("input", (event) => {
-      
-    });
+    document.addEventListener("keydown", this.#onKeyDown);
+    document.addEventListener("keyup", this.#onKeyUp);
+    this.#inputEl.addEventListener("input", this.#onInput);
+  }
+
+  #onInput(event) {
+    event.target.value = event.target.value.replace(
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/,
+      ""
+    );
+  }
+
+  #onKeyDown(event) {
+    this.#inputGrouptEl.classList.toggle(
+      "error",
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
+    );
+    
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.add("active");
+  }
+  
+  #onKeyUp(event) {
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.remove("active");
   }
 
   #onChangeTheme(event) {
